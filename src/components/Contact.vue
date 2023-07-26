ab79327bbc6b6029b021a327a773ce56dd7f3983
Oliver Cox <github@konx.dev>
2023-07-26 22:49:21 +0100

    removal of husky and staging linting

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,20 @@
 <template>
 	<div class="mb-10">
-		<h2 class="text-brand-white text-center font-playfair text-2xl md:text-3xl mb-5 md:mb-8">Contact me</h2>
-		<div class="flex justify-center mb-5 md:mb-8">
-			<a class="bg-brand-teal text-brand-navy font-opensans py-4 px-8 rounded" href="mailto:hire@konx.dev"
+		<h2 class="mb-5 text-center font-playfair text-2xl text-brand-white md:mb-8 md:text-3xl">Contact me</h2>
+		<div class="mb-5 flex justify-center md:mb-8">
+			<a class="rounded bg-brand-teal px-8 py-4 font-opensans text-brand-navy" href="mailto:hire@konx.dev"
 				>hello@konx.dev</a
 			>
 		</div>
 		<div class="flex justify-center gap-3">
 			<a href="https://www.linkedin.com/in/oliverjamescox/" aria-label="linkedin" rel="nofollow" target="_blank">
 				<font-awesome-icon
-					class="text-xl md:text-2xl text-brand-white hover:text-brand-teal"
+					class="text-xl text-brand-white hover:text-brand-teal md:text-2xl"
 					:icon="['fab', 'linkedin']" />
 			</a>
 			<a href="https://github.com/konx-dev" aria-label="github" rel="nofollow" target="_blank">
 				<font-awesome-icon
-					class="text-xl md:text-2xl text-brand-white hover:text-brand-teal"
+					class="text-xl text-brand-white hover:text-brand-teal md:text-2xl"
 					:icon="['fab', 'github']" />
 			</a>
 		</div>
