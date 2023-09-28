ea352511d309fe5c7fd40f0f42eaba437d37ccc4
Oliver Cox <github@konx.dev>
2023-09-28 21:56:06 +0100

    fixing deepsource issues

--- src/components/RenderHtml.vue
+++ src/components/RenderHtml.vue
@@ -1,56 +1,57 @@
 <!-- solution found here: https://stackoverflow.com/questions/46782359/vue-js-v-html-alternative -->
 
 <script>
-import { h } from "vue";
+	import { h } from "vue";
 
-export default {
-  props: {
-    html: {
-      type: String,
-      required: true,
-    },
-    tags: {
-      type: Array,
-      default: () => ["p"],
-    },
-  },
-  setup(props) {
-    const html = parseHTML(props.html);
+	const parseHTML = (html) => {
+		const template = document.createElement("template");
+		template.innerHTML = html;
+		return template.content;
+	};
 
-    // return the render function
-    return () => {
-      const output = [];
-      html.childNodes.forEach((childNode) => {
-        output.push(renderNode(childNode, props.tags));
-      });
-      if (output.length === 1) {
-        return output[0];
-      } else if (output.length === 0) {
-        return "";
-      }
-      return output;
-    };
-  },
-};
+	const renderNode = (node, tags) => {
+		const nodeType = node.nodeType;
+		if (nodeType === 1) {
+			const nodeName = node.nodeName.toLocaleLowerCase();
+			if (tags.includes(nodeName)) {
+				const children = [];
+				node.childNodes.forEach((childNode) => {
+					children.push(renderNode(childNode, tags));
+				});
+				return h(nodeName, children);
+			}
+		}
+		return node.textContent;
+	};
 
-const parseHTML = (html) => {
-  var template = document.createElement("template");
-  template.innerHTML = html;
-  return template.content;
-};
+	export default {
+		name: "RenderHtml",
+		props: {
+			html: {
+				type: String,
+				required: true,
+			},
+			tags: {
+				type: Array,
+				default: () => ["p"],
+			},
+		},
+		setup(props) {
+			const html = parseHTML(props.html);
 
-const renderNode = (node, tags) => {
-  const nodeType = node.nodeType;
-  if (nodeType === 1) {
-    const nodeName = node.nodeName.toLocaleLowerCase();
-    if (tags.includes(nodeName)) {
-      const children = [];
-      node.childNodes.forEach((childNode) => {
-        children.push(renderNode(childNode, tags));
-      });
-      return h(nodeName, children);
-    }
-  }
-  return node.textContent;
-};
+			// return the render function
+			return () => {
+				const output = [];
+				html.childNodes.forEach((childNode) => {
+					output.push(renderNode(childNode, props.tags));
+				});
+				if (output.length === 1) {
+					return output[0];
+				} else if (output.length === 0) {
+					return "";
+				}
+				return output;
+			};
+		},
+	};
 </script>
